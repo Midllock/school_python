6e1091e1ef4e7168edc4dbe38df4dd17d9fbe179
Midllock <119570624+Midllock@users.noreply.github.com>
2023-11-20 19:47:06 +0100

    úkol

--- volná práce.py
+++ volná práce.py
@@ -30,7 +30,16 @@ for i in range (len(numbers) - 1):
     if numbers[i] > numbers[i + 1]:
         numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
 print(numbers)
-"""""""""
+
+numbers = [4,5,3,7,8,2,1,6]
+for i in range (len(numbers) - 1):
+    if numbers[i] > numbers[i + 1]:
+        numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
+print(numbers)
+
+n = 7
+for i in range (1, 11):
+    print(i, "x", n, "=", i * n)
 
 import math
 player = [2,5]
@@ -39,3 +48,23 @@ a = (player[0]- enemy [0])
 b = (player[1]- enemy [1])
 c = math.sqrt(a**2 + b**2)
 print(c)
+"""""""""
+
+znamky = [[3, 1], [3, 2], [3, 1], [10, 3], [4, 4]]
+
+celkova_vaha = 0
+vazeny_prumer_citatel = 0
+
+for znamka in znamky:
+    celkova_vaha += znamka[0]
+    vazeny_prumer_citatel += znamka[0] * znamka[1] 
+
+vazeny_prumer = vazeny_prumer_citatel / celkova_vaha
+
+nevažený_průměr = sum(znamka[1] for znamka in znamky) /len(znamky)
+
+rozdil = vazeny_prumer - nevažený_průměr
+
+print(f"Celkový vážený průměr: {vazeny_prumer}")
+print(f"Celkový nevážený průměr: {nevažený_průměr}")
+print(f"Rozdíl mezi průměry: {rozdil}")
