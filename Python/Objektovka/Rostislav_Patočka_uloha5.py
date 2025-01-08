507aaa8755e3eeb6a7c0d5de13ad451d24348b5a
Midllock <119570624+Midllock@users.noreply.github.com>
2025-01-08 10:16:28 +0100

    Update Rostislav_Patočka_uloha5.py

--- Python/Objektovka/Rostislav_Patočka_uloha5.py
+++ Python/Objektovka/Rostislav_Patočka_uloha5.py
@@ -15,20 +15,20 @@ class Auto(Vozidlo):
         self.pocet_dveri = pocet_dveri
     
     def start(self):
-        return f"Auto {self.znacka} startuje se {self.pocet_dveri} dveřmi."
+        return f"Auto {self.znacka} se značkou {self.model} startuje se {self.pocet_dveri} dveřmi."
 class Motorka(Vozidlo):
     def __init__(self, znacka, model, typ_motorky):
         super().__init__(znacka, model)
         self.typ_motorky = typ_motorky
     
     def start(self):
-        return f"Motorka {self.znacka} typu {self.typ_motorky} startuje."
+        return f"Motorka {self.znacka} se značkou {self.model} typu {self.typ_motorky} startuje."
     
 auto1 = Auto("Škoda", "Octavia", 4)
 auto2 = Auto("Volkswagen", "Golf", 5)
     
 motorka1 = Motorka("Honda", "CBR500R", "sportovní")
-motorka2 = Motorka("Yamaha", "MT-07", " naked bike")
+motorka2 = Motorka("Yamaha", "MT-07", "naked bike")
     
 print(auto1.start())  # Auto Škoda startuje se 4 dveřmi.
 print(auto2.start())  # Auto Volkswagen startuje se 5 dveřmi.
